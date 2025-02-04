ccf02530eea8962001fcf26b21348b3f0d6c7fcf
Yaşar Veli <veliyasar04@gmail.com>
2025-02-04 10:35:27 +0100

    last push

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-let honger = 55;
+let honger = 60;
 let slaap = 40;
-let plezier = 60;
-let message;
+let plezier = 35;
+let message = "";
 
 const hongerElement = document.getElementById("honger");
 const slaapElement = document.getElementById("slaap");
 const plezierElement = document.getElementById("plezier");
-
+const obitoImage = document.getElementById("obito-image");
 
 function eten() {
     if (honger < 100) {
@@ -16,15 +16,13 @@ function eten() {
     }
 }
 
-
 function slapen() {
     if (slaap < 100) {
-        slaap += 10; //
+        slaap += 10;
         if (slaap > 100) slaap = 100; 
         slaapElement.innerText = `${slaap}%`; 
     }
 }
-
 
 function verhoog_plezier() {
     if (plezier < 100) {
@@ -34,9 +32,14 @@ function verhoog_plezier() {
     }
 }
 
+let isDead = false;  
 
 setInterval(function() {
    
+    if (isDead) {
+        return;
+    }
+
     if (honger > 0) {
         honger -= 1;
         hongerElement.innerText = `${honger}%`;
@@ -50,19 +53,30 @@ setInterval(function() {
     if (plezier > 0) {
         plezier -= 1;
         plezierElement.innerText = `${plezier}%`;
-        let message ="";
-        
-    if (honger < 50) {
-        document.getElementById("message").innerText = "lol";
-
     }
 
+    if (honger < 50 && slaap > 20 && plezier > 20) {
+        message = "ik heb honger";
+        obitoImage.src = "hungry_panda.png"; 
     }
-
     
-   
-    // document.getElementById("statistieken").innerText = 
-    //     `Honger - ${honger}% | Slaap - ${slaap}% | verhoog_plezier - ${plezier}%`;
+    if (slaap < 20) {
+        message = "ik heb slaap";
+        obitoImage.src = "sleepy_panda.png";
+    }
+    if (plezier < 10) {
+        message = "ik wil spelen";
+        obitoImage.src = "happy_panda.png"; 
+    } 
+    
+  
+    if (honger === 0 && slaap === 0 && plezier === 0) {
+        message = "De panda is dood...";
+        obitoImage.src = "dead_panda.png";  
+        isDead = true;  
+    }
 
-}, 1000); 
+    document.getElementById("message").innerText = message;
+
+}, 1000);
 
